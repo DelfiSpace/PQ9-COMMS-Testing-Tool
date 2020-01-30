313c5ff9d85ce05e474c5957eebdd44f5c287b44
CasperBroekhuizen <broekhuizen.casper@gmail.com>
2020-01-30 15:36:33 +0100

    updated

--- AX25_Detector_FEC.py
+++ AX25_Detector_FEC.py
@@ -8,9 +8,10 @@ class AX25Detector:
 
     def __init__(self):
         super().__init__()
+        self.startlengthcheck = 5
+        self.taillengthcheck = 1
         self.state = 0
-        self.state = 0
-        self.flagbuffer = collections.deque(maxlen = 8)
+        self.flagbuffer = collections.deque(maxlen = self.startlengthcheck*8)
         self.msgBuffer = []
         self.msgBuffer_raw = []
         self.bitCount = 0
@@ -24,7 +25,7 @@ class AX25Detector:
             bit = self.g3ruhEncoder.DescrambleBit(self.g3ruhEncoder.NRZIDecodeBit(inbit))
 
             self.flagbuffer.append(bit)
-            if compareByte(bits2byte(list(self.flagbuffer)), int("0x7E", 16)) <= 1:
+            if compareBytes(bits2bytes(list(self.flagbuffer)), self.startlengthcheck*[int("0x7E", 16)]) <= self.startlengthcheck:
                 #print("FLAG DETECTED")
                 self.msgBuffer = []
                 self.msgBuffer_raw = []
@@ -56,11 +57,11 @@ class AX25Detector:
             ##receiving message
             self.msgBuffer.append(bit)
             self.bitCount = self.bitCount + 1
-            if bits2byte(list(self.msgBuffer[-8:])) == int("0x7E", 16):
+            if compareBytes(bits2bytes(list(self.msgBuffer[-self.taillengthcheck*8:])), self.taillengthcheck*[int("0x7E", 16)]) <= 0:
                 ##end sequence detected!
 
                 ##destuff msg:
-                self.msgBuffer = AX25_Encoder.AX25Encoder.DeStuffBits(self.msgBuffer[:-8])
+                self.msgBuffer = AX25_Encoder.AX25Encoder.DeStuffBits(self.msgBuffer[:-8*self.taillengthcheck])
                 if len(self.msgBuffer)%8 == 0 and len(self.msgBuffer) > 18*8:
                     receivedBytes = flipbytes(bits2bytes(self.msgBuffer))
                     if (AX25_Encoder.AX25Encoder.CheckFCS(receivedBytes)):
@@ -70,6 +71,72 @@ class AX25Detector:
                     else:
                         print("AX25 MSG FCS FAILED!  ", end = "")
                         self.iterateBitFlip(self.msgBuffer_raw[:-8])
+                self.msgBuffer = []
+                self.bitCount = 0
+                self.state = 0
+            if self.bitCount >= (512+18)*8:
+                ##msg too long, hence not a msg
+                #print("MSG too long!")
+                self.msgBuffer = []
+                self.bitCount = 0
+                self.state = 0
+
+    def queBit_FEC_FIXED_LEN(self, y, LEN):
+        if self.state == 0:
+            ##Detect Flag
+            inbit = soft2hardBit(y)
+            bit = self.g3ruhEncoder.DescrambleBit(self.g3ruhEncoder.NRZIDecodeBit(inbit))
+            self.flagbuffer.append(bit)
+            if compareBytes(bits2bytes(list(self.flagbuffer)), self.startlengthcheck*[int("0x7E", 16)]) <= 0:
+                #print("FLAG DETECTED")
+                self.msgBuffer = []
+                self.msgBuffer_raw = []
+                self.state = 1
+                self.g3ruhEncoder.SaveState()
+
+        elif self.state == 1:
+            inbit = soft2hardBit(y)
+            bit = self.g3ruhEncoder.DescrambleBit(self.g3ruhEncoder.NRZIDecodeBit(inbit))
+            ##Check for message start
+            self.msgBuffer_raw.append(y)
+            self.msgBuffer.append(bit)
+            self.bitCount = self.bitCount + 1
+            if (self.bitCount == 8):
+                if compareByte(bits2byte(list(self.msgBuffer)), int("0x7E", 16)) <= 1:
+                    ##first incoming byte is another flag, so drop this byte
+                    #print("Another flag!")
+                    self.msgBuffer = []
+                    self.msgBuffer_raw = []
+                    self.bitCount = 0
+                    self.g3ruhEncoder.SaveState()
+                else:
+                    ##first incoming byte was not a flag, so the message has started!
+                    #print("MESSAGE START!")
+                    self.state = 2
+        elif self.state == 2:
+            inbit = soft2hardBit(y)
+            bit = self.g3ruhEncoder.DescrambleBit(self.g3ruhEncoder.NRZIDecodeBit(inbit))
+            self.msgBuffer_raw.append(y)
+            ##receiving message
+            self.msgBuffer.append(bit)
+            self.bitCount = self.bitCount + 1
+            
+            if compareBytes(bits2bytes(list(self.msgBuffer[-self.taillengthcheck*8:])), self.taillengthcheck*[int("0x7E", 16)]) <= 0:
+                #print("hm!")
+                ##end sequence detected!
+
+                ##destuff msg:
+                self.msgBuffer = AX25_Encoder.AX25Encoder.DeStuffBits(self.msgBuffer[:-8*self.taillengthcheck])
+                if len(self.msgBuffer)%8 == 0 and len(self.msgBuffer) > 18*8:
+                    receivedBytes = flipbytes(bits2bytes(self.msgBuffer))
+                    if len(receivedBytes) == 53:
+                        if (AX25_Encoder.AX25Encoder.CheckFCS(receivedBytes)):
+                            self.packetCount += 1
+                            print("AX25 MSG RECEIVED! | NR: %2d | LEN: %2d | MSG: " % (self.packetCount, int(len(receivedBytes))), end="")
+                            print(''.join('{:02X} '.format(x) for x in receivedBytes))
+                        else:
+                            print("AX25 MSG FCS FAILED!  ", end = "")
+                            self.iterateBitFlip(self.msgBuffer_raw[:-8])
                 self.msgBuffer = []
                 self.bitCount = 0
                 self.state = 0
--- AX25_FEC_TEST.py
+++ AX25_FEC_TEST.py
@@ -9,10 +9,10 @@ g3ruhEncoder = NRZI_G3RUH_Encoder.G3RUHEncoder()
 ax25Encoder = AX25_Encoder.AX25Encoder()
 ax25Detector = AX25_Detector_FEC.AX25Detector()
 
-for i in range(0,150):
+for i in range(0,1):
     ##  SENDER
     pilot_seq = (50*[int("0x7E", 16)])
-    tail_seq = [int("0x7E", 16), int("0x7E", 16)]
+    tail_seq = (20*[int("0x7E", 16)])
     data = [int("0x82", 16), int("0x98", 16),int("0x98", 16), int("0x40", 16),int("0x40", 16),int("0x40", 16),int("0xE0", 16),int("0x40", 16),int("0x40", 16),int("0x40", 16),int("0x40", 16),int("0x40", 16),int("0x40", 16),int("0x61", 16), int("0x03", 16), int("0xF0", 16)]
     data += [random.randint(0,255) for i in range(0,35)]
     data.extend(ax25Encoder.CalculateFCS(data))
@@ -24,7 +24,7 @@ for i in range(0,150):
 
 
     ## CHANNEL 16 8 6 4 2
-    EbN = 7 #dB
+    EbN = 25 #dB
     channelBits = np.array(txBits)
     SNR_lin = 10**(EbN/10)
     No = 1.0/SNR_lin
@@ -37,5 +37,6 @@ for i in range(0,150):
     Lc = 2/(sigma**2)
     rxBits = Lc * channelBits
     for rxBit in rxBits:
-        ax25Detector.queBit_FEC(rxBit)
+        ax25Detector.queBit_FEC_FIXED_LEN(rxBit,53)
+
 
